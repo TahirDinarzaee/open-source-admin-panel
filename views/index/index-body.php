8dc1aeec2c0bd0a97edcae1106997395b69479cd
TahirD <tahirdinarzaee@outlook.com>
2020-02-12 19:17:43 +0000

    content added to the index body view

---
+++ views/index/index-body.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Index-body view</h4>
+            </div>
+        </div>
+    </div>
+</div>
